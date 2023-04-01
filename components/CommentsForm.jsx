8630c7bd5ea95748cac236be278c1863dcd6929c
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2023-04-01 21:00:34 +0200

    Termino formulario para los comentarios y los muestro debajo

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 
+import { submitComment } from "../services";
+
 const CommentsForm = ({ slug }) => {
   const [error, setError] = useState(false);
   const [localStorage, setLocalStorage] = useState(null);
@@ -9,24 +11,46 @@ const CommentsForm = ({ slug }) => {
   const emailEl = useRef();
   const storeDataEl = useRef();
 
+  useEffect(() => {
+    nameEl.current.value = window.localStorage.getItem('name');
+    emailEl.current.value = window.localStorage.getItem('email');
+  })
+
   const handleCommentSubmission = () => {
     setError(false);
 
     const { value: comment } = commentEl.current;
     const { value: name } = nameEl.current;
     const { value: email } = emailEl.current;
+    const { checked: storeData } = storeDataEl.current;
 
     if(!comment || !name || !email ) {
       setError(true);
       return;
     }
 
-    //commentObj
+    const commentObj = { name, email, comment, slug };
+
+    if(storeData) {
+      window.localStorage.setItem('name', name);
+      window.localStorage.setItem('email',email);
+    } else {
+      window.localStorage.removeItem('name', name);
+      window.localStorage.removeItem('email', email);
+    }
+
+    submitComment(commentObj)
+    .then((res) => {
+      setShowSuccessMessage(true);
+      setTimeout(() => {
+        setShowSuccessMessage(false);
+      }, 3000);
+    })
   };
 
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
-      <h3 className="text-xl mb-8 font-semibold border-b pb-4">CommentsForm</h3>
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Leave a Reply</h3>
       <div className="grid grid-cols-1 gap-4 mb-4">
         <textarea
           ref={commentEl}
@@ -50,6 +74,12 @@ const CommentsForm = ({ slug }) => {
           placeholder="Email"
           name="email"
         />
+      </div>
+      <div className="grid grid-cols-1 gap-4 mb-4">
+        <div>
+          <input ref={storeDataEl} type="checkbox" id="storeData" name="storeData" value="true"/>
+          <label className="text-gray-500 cursor-pointer ml-2" htmlFor="storeData">Save my e-amil and name for the next time I comment.</label>
+        </div>
       </div>
       {error && (
         <p className="text-xs text-red-500">All fields are required.</p>
